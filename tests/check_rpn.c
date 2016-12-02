5cbc87d1228b0bd3bffac1287122110277cc0efc
Erik Christensen <christensenep@gmail.com>
2016-12-02 12:49:50 -0500

    Test converting a single operand

--- tests/check_rpn.c
+++ tests/check_rpn.c
@@ -4,9 +4,16 @@
 
 START_TEST(i2p_should_convert_empty_string_to_empty_string)
 {
-    const char* infixString = "";
-    char* postfixString = infix_to_postfix(infixString);
+    char* postfixString = infix_to_postfix("");
     ck_assert_str_eq(postfixString, "");
+    free(postfixString);
+}
+END_TEST
+
+START_TEST(i2p_should_convert_single_operand)
+{
+    char* postfixString = infix_to_postfix("a");
+    ck_assert_str_eq(postfixString, "a");
     free(postfixString);
 }
 END_TEST
@@ -17,6 +24,7 @@ Suite* rpn_test_suite(void)
     TCase* tcase_infix_to_postfix = tcase_create("Infix To Postfix");
 
     tcase_add_test(tcase_infix_to_postfix, i2p_should_convert_empty_string_to_empty_string);
+    tcase_add_test(tcase_infix_to_postfix, i2p_should_convert_single_operand);
     suite_add_tcase(rpnSuite, tcase_infix_to_postfix);
 
     return rpnSuite;
